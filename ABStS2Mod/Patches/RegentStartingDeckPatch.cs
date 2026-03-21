04f28769acc6c402c63c0341ecf63445fbacc457
kjqwer <2990346238@qq.com>
2026-03-21 18:14:36 +0800

    增加 劫掠者暴徒

--- ABStS2Mod/Patches/RegentStartingDeckPatch.cs
+++ ABStS2Mod/Patches/RegentStartingDeckPatch.cs
@@ -19,7 +19,7 @@ public static class RegentStartingDeckPatch
         if (!newDeck.Any(card => card.Id == ModelDb.Card<SoulCapture>().Id))
         {
             newDeck.Add(ModelDb.Card<SoulCapture>());
-            newDeck.Add(ModelDb.Card<SoulMonsterFlyconid>());
+            newDeck.Add(ModelDb.Card<SoulMonsterBruteRubyRaider>());
         }
         // if (!newDeck.Any(card => card.Id == ModelDb.Card<SoulCaptureTest>().Id))
         // {
--- Cards/MonsterSouls/SoulMonsterBruteRubyRaider.cs
+++ Cards/MonsterSouls/SoulMonsterBruteRubyRaider.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using BaseLib.Abstracts;
 using BaseLib.Utils;
@@ -7,16 +8,39 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace ABStS2Mod.Cards.MonsterSouls;
 
 [Pool(typeof(ColorlessCardPool))]
-public sealed class SoulMonsterBruteRubyRaider() : CustomCardModel(0, CardType.Skill, CardRarity.Event, TargetType.Self)
+public sealed class SoulMonsterBruteRubyRaider() : CustomCardModel(1, CardType.Attack, CardRarity.Event, TargetType.AnyEnemy)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[] { new CardsVar(1) };
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new DamageVar(12m, ValueProp.Move),
+        new CalculationExtraVar(10m),
+        new CardsVar(1)
+    };
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        decimal bonusDamage = CountStatusCardsInHand(Owner) * DynamicVars.CalculationExtra.BaseValue;
+        var attack = await DamageCmd.Attack(DynamicVars.Damage.BaseValue + bonusDamage)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_blunt", null, "blunt_attack.mp3")
+            .Execute(choiceContext);
+
+        if (attack.Results.Any(result => result.TotalDamage > 0))
+        {
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+        }
+    }
+
+    private static int CountStatusCardsInHand(MegaCrit.Sts2.Core.Entities.Players.Player owner)
+    {
+        return PileType.Hand.GetPile(owner).Cards.Count(c => c.Type == CardType.Status);
     }
 }
