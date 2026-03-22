e944f7d3b0513fc4bee5470d5c494c03a4b4de08
kjqwer <2990346238@qq.com>
2026-03-22 17:10:36 +0800

    增加暗港3张卡

--- Cards/MonsterSouls/SoulMonsterCalcifiedCultist.cs
+++ Cards/MonsterSouls/SoulMonsterCalcifiedCultist.cs
@@ -5,18 +5,34 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace ABStS2Mod.Cards.MonsterSouls;
 
 [Pool(typeof(ColorlessCardPool))]
-public sealed class SoulMonsterCalcifiedCultist() : CustomCardModel(0, CardType.Skill, CardRarity.Event, TargetType.Self)
+public sealed class SoulMonsterCalcifiedCultist() : CustomCardModel(2, CardType.Power, CardRarity.Event, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[] { new CardsVar(1) };
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new PowerVar<RitualPower>(2m)
+    };
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => new IHoverTip[]
+    {
+        HoverTipFactory.FromPower<RitualPower>(),
+        HoverTipFactory.FromPower<StrengthPower>()
+    };
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+        await PowerCmd.Apply<RitualPower>(Owner.Creature, DynamicVars["RitualPower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["RitualPower"].UpgradeValueBy(1m);
     }
 }
